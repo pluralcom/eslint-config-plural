ea7cc446513810e9a1c1f100a8c8fd30709ebb10
monicapatel1512 <113090067+monicapatel1512@users.noreply.github.com>
2022-12-08 16:19:26 +0530

    ENG-267 implement function-component rule and update version

--- rules/base.js
+++ rules/base.js
@@ -4,5 +4,11 @@ module.exports = {
     'no-underscore-dangle': 0,
     'arrow-parens': 0,
     'no-confusing-arrow': 0,
+    "react/function-component-definition": [
+      2,
+      {
+        "namedComponents": "arrow-function"
+      }
+    ]
   },
 };
