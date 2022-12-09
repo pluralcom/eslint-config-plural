ca0679eb5824350c27e2d9cb62dd1d79418e088f
monicapatel1512 <113090067+monicapatel1512@users.noreply.github.com>
2022-12-09 13:48:37 +0530

    ENG-267 implement rules

--- rules/base.js
+++ rules/base.js
@@ -4,11 +4,5 @@ module.exports = {
     'no-underscore-dangle': 0,
     'arrow-parens': 0,
     'no-confusing-arrow': 0,
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-      },
-    ],
   },
 };
