b357859872e8879834f4c9b84bbc845183f0218a
Tokyo <ahmed.tokyo1@gmail.com>
2023-01-13 15:03:52 +0000

    :bug: fix lint

--- rules/base.js
+++ rules/base.js
@@ -1,19 +1,19 @@
 module.exports = {
   rules: {
-    "arrow-parens": 0,
+    'arrow-parens': 0,
     camelcase: 0,
-    "no-confusing-arrow": 0,
-    "no-underscore-dangle": 0,
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'no-confusing-arrow': 0,
+    'no-underscore-dangle': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "": "never",
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-        json: "never",
+        ': 'never',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
       },
     ],
   },
